5d67374ec0ea89f53a38ba4984cbd53365d6cdfa
Parise MohanKumar <mohankumar.parise523@gmail.com>
2024-08-09 11:16:37 +0530

    Initial Commit

---
+++ src/main/java/com/sarees/handlooms/HandloomsApplication.java
@@ -0,0 +1,13 @@
+package com.sarees.handlooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandloomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandloomsApplication.class, args);
+	}
+
+}
